fd6ceec5da0b6af69feba73c04be72df614af79b
Chanmin Lee <lee00837@algonquinlive.com>
2025-03-22 10:00:06 -0600

    Added footer

--- src/Components/MainPage.tsx
+++ src/Components/MainPage.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './Nav/Navbar.tsx';
+import Cover from './Cover/Cover.tsx';
+import Footer from './Footer/Footer.tsx';
+import {HiveDental} from "./Model/HiveInfo.ts"
 
-const MainPage: React.FC = () => {
+interface MainPageProps {
+    info: HiveDental;
+}
+const MainPage: React.FC<MainPageProps> = ({ info }) => {
     return (
       <>
         <Navbar />
+        <Cover />
+        <Footer info={info}/>
       </>
     );
   };
---
+++ src/Components/Model/HiveInfo.ts
@@ -0,0 +1,19 @@
+export type HiveDental = {
+    name: string;
+    address: Address;
+    phoneNumber: string;
+    hours: OperatingHours[];
+}
+
+export type Address = {
+    streetNumber: string;
+    unitNumber?: string;
+    city: string;
+    province: string;
+    zipCode: string;
+}
+
+export type OperatingHours = {
+    day: string;
+    time: string;
+}
